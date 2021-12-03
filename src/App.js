b74c1c22d46f7e4d87ce042477e1aa535f7a9a67
Gurban Gurbanov <qurban.qurbanov93@gmail.com>
2021-12-03 22:54:58 +0400

    Added /docs folder for github pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={props.callFunction}>Break the world</button>
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
 import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+    dsn: "https://beeec1430c6241ce8f83aff855a458da@o1083339.ingest.sentry.io/6092995",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
